2fbb86bf56a03ca0a70fcbd4af38fea4dff83ef4
Marek Snyder <mr.snyder31@gmail.com>
2022-12-12 10:25:53 -0500

    complete

---
+++ main.js
@@ -0,0 +1,58 @@
+/* build a calculator that can add, subtract, divide, muliplty, and square numbers
+build functions that take in two numbers as parameters, 
+        applies the math process, and returns the result
+ */
+
+        const add = (num1, num2) => {
+            output = num1 + num2;
+            return console.log(output);
+        };
+
+        const subtract = (num1, num2) => {
+            output = num1 - num2;
+            return console.log(output);
+        };
+
+        const divide = (num1, num2) => {
+            output = num1 / num2;
+            return console.log(output);
+        };
+
+        const multiply = (num1, num2) => {
+            output = num1 * num2;
+            return console.log(output);
+        };
+
+        const squared = (num1) => {
+            output = num1 * num1;
+            return console.log(output);
+        };
+
+       /* 
+        
+      getting hint. math needs to be refreshed.
+      reduce
+       (0.25x * 4) + (0.75x * 2) = 60
+        1x + 1.5x = 60
+        2.5x = 60
+        x=24
+       */
+       
+       multiply(0.25, 4)
+       multiply(0.75, 2)
+       add(1,0.15)
+       divide(60,2.5)
+       
+       const totalAnimals = () => {
+        let output =
+        multiply(.25,4) + multiply(0.75,2) / 60
+        console.log(output);
+       }
+
+     
+
+
+
+
+
+
